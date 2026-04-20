0d45e5ec75acc3df8b9e0f649836fe1aad0ba8f8
giang223 <giangdv22032001@gmail.com>
2026-04-20 19:20:20 +0700

    Create Java-Core Folder

---
+++ Java_Core/assignment_1/src/main/java/Account.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Account {
+    public int id;
+    public String username;
+    public String fullName;
+    Department department;
+    Position position;
+    LocalDate createDate;
+}
